b293d77f03855ed8380c1726cb58071f272c8edc
ruaa <ruaa.reda99@gmail.com>
2023-06-25 23:10:24 +0300

    create header component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import ForgetPassword from "./pages/ForgetPassword";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<Signin />} />
