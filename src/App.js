d9cc578f3638fbd7d418ff7094f296fbfc243d2c
ruaa <ruaa.reda99@gmail.com>
2023-06-25 20:53:49 +0300

    create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgetPassword from "./pages/ForgetPassword";
+import Offers from "./pages/Offers";
 function App() {
   return (
     <>
-      <h1 className="text-2xl bg-red-300">hello</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
